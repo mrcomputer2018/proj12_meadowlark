dc692e125fe678c3a58c6f08665a86c65a8f69d5
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-01-18 08:00:15 -0300

    acerto da array de sorteio

--- site/lib/fortune.js
+++ site/lib/fortune.js
@@ -1,11 +1,11 @@
 // Modulo Fortunes
 // Array fortunes
 const fortuneCookies = [
-    "Vença seus medos ou eles vão te conquistar.",
-    "Os rios precisam de nascentes.",
-    "Não tenha medo do que você não conhece.",
-    "Você terá uma agradável surpresa.",
-    "Sempre que possível, mantenha as coisas simples.",
+    "...Vença seus medos ou eles vão te conquistar...",
+    "...Os rios precisam de nascentes...",
+    "...Não tenha medo do que você não conhece...",
+    "...Você terá uma agradável surpresa...",
+    "...Sempre que possível, mantenha as coisas simples....",
 ]
 
 // Exportando Glbalmente Fortune
