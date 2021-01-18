6a32fd5b4f71a78e65a26aebb7389ae0a016e6a1
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-01-18 08:40:37 -0300

    Adicionando teste das outras rotas

--- site/lib/_tests_/handlers.test.js
+++ site/lib/_tests_/handlers.test.js
@@ -1,10 +1,41 @@
 // Importando HANDLERS
 const handlers = require('../handlers') 
 
+// Teste da Rota HOME
 test('home page renders', () => { 
     const req = {} 
     const res = { render: jest.fn() } 
     handlers.home(req, res) 
     expect(res.render.mock.calls[0][0]).toBe('home') 
+})
+
+// Teste da Rota ABOUT
+test('about page renders with fortune', () => { 
+    const req = {} 
+    const res = { render: jest.fn() } 
+    handlers.about(req, res) 
+    expect(res.render.mock.calls.length).toBe(1) 
+    expect(res.render.mock.calls[0][0]).toBe('about')
+    expect(res.render.mock.calls[0][1]).toEqual(expect.objectContaining({ fortune: expect.stringMatching(/\W/), 
+    })) 
+})
+
+// Teste da Rota 404
+test('404 handler renders', () => { 
+    const req = {} 
+    const res = { render: jest.fn() } 
+    handlers.notFound(req, res) 
+    expect(res.render.mock.calls.length).toBe(1) 
+    expect(res.render.mock.calls[0][0]).toBe('404') }) 
+
+// Teste da Rota 500
+test('500 handler renders', () => { 
+    const err = new Error('some error') 
+    const req = {} 
+    const res = { render: jest.fn() } 
+    const next = jest.fn() 
+    handlers.serverError(err, req, res, next) 
+    expect(res.render.mock.calls.length).toBe(1) 
+    expect(res.render.mock.calls[0][0]).toBe('500') 
 })
 
